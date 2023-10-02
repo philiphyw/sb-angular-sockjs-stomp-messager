44b1a1071bc43a30dfff28cd926c988eba734396
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2023-10-02 09:06:54 -0400

    initial commit

---
+++ src/main/java/com/outlook/philiphyw/sbangularsockjsstompmessager/SbAngularSockjsStompMessagerApplication.java
@@ -0,0 +1,13 @@
+package com.outlook.philiphyw.sbangularsockjsstompmessager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAngularSockjsStompMessagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAngularSockjsStompMessagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outlook/philiphyw/sbangularsockjsstompmessager/SbAngularSockjsStompMessagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outlook.philiphyw.sbangularsockjsstompmessager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAngularSockjsStompMessagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
